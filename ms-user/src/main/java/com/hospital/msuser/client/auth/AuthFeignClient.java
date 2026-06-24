0602fa787fd6ff96044739a3fcd40d1f99c03c2a
Alorca <aa.lorca@duocuc.cl>
2026-06-24 14:26:15 -0400

    refactor: eliminar clases deprecated UserClient, AuthFeignClient, AuthClient

--- ms-user/src/main/java/com/hospital/msuser/client/auth/AuthFeignClient.java
+++
@@ -1,9 +0,0 @@
-package com.hospital.msuser.client.auth;
-
-/**
- * @deprecated Reemplazado por {@link AuthRestClient} (Spring RestClient, compatible con Java 25).
- * Eliminar con: git rm src/main/java/com/hospital/msuser/client/auth/AuthFeignClient.java
- */
-@Deprecated
-public class AuthFeignClient {
-}
