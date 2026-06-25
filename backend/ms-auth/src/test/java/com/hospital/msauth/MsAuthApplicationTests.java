df62141a428987d7b5dda7d3b7df04d1240f93d2
Alorca <aa.lorca@duocuc.cl>
2026-06-25 10:18:54 -0400

    chore: gradle 9.1.0, playwright tests, fix MsAuthApplicationTests

--- backend/ms-auth/src/test/java/com/hospital/msauth/MsAuthApplicationTests.java
+++ backend/ms-auth/src/test/java/com/hospital/msauth/MsAuthApplicationTests.java
@@ -5,10 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(properties = {
-    "security.jwt.private-key-path=/Users/elian/Documents/Ms-Auth/src/test/resources/keys/private_key.pem",
-    "security.jwt.public-key-path=/Users/elian/Documents/Ms-Auth/src/test/resources/keys/public_key.pem"
-})
+@TestPropertySource(locations = "classpath:application.yml")
 class MsAuthApplicationTests {
 
     @Test
